e716c78a5bf62415fd656575f79f22e723cc2e4a
tonykai <tony1992040417@gmail.com>
2014-11-07 11:43:11 +0800

    [Bugs][Fixed][SM]Modified error link

--- SchoolMaster/PlanViewMain.aspx.cs
+++ SchoolMaster/PlanViewMain.aspx.cs
@@ -295,7 +295,7 @@ public partial class SchoolMaster_PlanViewMain : System.Web.UI.Page
     }
     protected void btnBack_Click1(object sender, EventArgs e)
     {
-        Response.Redirect("PlanList.aspx");
+        Response.Redirect("PlanViewList.aspx");
     }
     protected void LkbPlanItem1_Click(object sender, EventArgs e)
     {
