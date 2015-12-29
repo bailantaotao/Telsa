ad30fef6b49829f0bcf52568f101fd6c7e2c226c
tonykai <tony1992040417@gmail.com>
2015-12-30 02:05:30 +0800

    [All][Func][Add]Help users to recover pwd to origin

---
+++ RecoverPassword.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RecoverPassword.aspx.cs" Inherits="RecoverPassword" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 32px;
+        }
+        .style1
+        {
+            width: 442px;
+        }
+    </style>
+</head>
+<body style="margin: 0; padding: 0; font-size: 14pt; border-top-style: none; font-family: Arial; border-right-style: none; border-left-style: none; border-bottom-style: none; background:url(<%= backgroundImage %>) no-repeat center top;">
+    
+    <form id="form1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server"/>
+    <div align="center">
+        <div style="height: 270px"></div>
+        <table style="width: 750px; height:291px; padding-left:50px ">
+            <tr style="height:auto;">
+                <td align="center" style="vertical-align: top">
+                    <table>
+                        <tr>
+                            <td class="style1">
+                                <asp:Label ID="Label1" runat="server" Text="省分:"></asp:Label>
+                                <asp:DropDownList ID="DdlProvince" runat="server" AutoPostBack="True" 
+                                    onselectedindexchanged="DdlProvince_SelectedIndexChanged"></asp:DropDownList>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="style1">
+                                <asp:Label ID="Label2" runat="server" Text="欲恢复之学校:"></asp:Label>
+                                <asp:DropDownList ID="DdlSchool" runat="server" Height="20px" Width="198px"></asp:DropDownList>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="style1">
+                                <asp:Button ID="BtnModify" runat="server" Text="确认恢复" 
+                                    onclick="BtnModify_Click" />
+                                <asp:Button ID="BtnCancel" runat="server" Text="取消恢复" 
+                                onclick="BtnCancel_Click" />
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <asp:Literal ID="ClientScriptArea" runat="server"></asp:Literal>    
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ RecoverPassword.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RecoverPassword : System.Web.UI.Page
+{
+    private string Query = string.Empty;
+
+    public string backgroundImage = Resources.Resource.ImgUrlBackground;
+
+    private enum DdlType
+    {
+        Province,
+        SchoolName
+    }
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        if (Session.Count == 0 || Session["UserName"].ToString() == "" || Session["UserID"].ToString() == "" || Session["ClassCode"].ToString() == "")
+            Response.Redirect("../SessionOut.aspx");
+
+        setDefault(DdlType.Province);
+        //DdlSchool.Items.Add(Resources.Resource.DdlTypeSchoolname);
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        setSchoolName();
+    }
+
+    private string SearchProvince()
+    {
+        string query = "select area.name from area where area.id='" + Session["Province"].ToString() + "'";
+        ManageSQL ms = new ManageSQL();
+        StringBuilder sb = new StringBuilder();
+        ms.GetOneData(query, sb);
+        return string.IsNullOrEmpty(sb.ToString()) ? "none" : sb.ToString();
+    }
+
+    private void setDefault(DdlType type)
+    {
+        switch (type)
+        {
+            case DdlType.Province:
+                setProvince();
+                break;
+            case DdlType.SchoolName:
+                setSchoolName();
+                break;
+        }
+    }
+    private void setProvince()
+    {
+        ManageSQL ms = new ManageSQL();
+        ArrayList data = new ArrayList();
+        Query = "select area.name from area where ID <= 31 order by id asc";
+        if (!ms.GetAllColumnData(Query, data))
+        {
+            DdlProvince.Items.Add("None");
+            return;
+        }
+
+        if (data.Count == 0)
+        {
+            DdlProvince.Items.Add("None");
+            return;
+        }
+        DdlProvince.Items.Add(Resources.Resource.DdlTypeProvince);
+        foreach (string[] province in data)
+        {
+            DdlProvince.Items.Add(province[0]);
+        }
+    }
+    private void setSchoolName()
+    {
+        ManageSQL ms = new ManageSQL();
+        ArrayList data = new ArrayList();
+        StringBuilder sb = new StringBuilder();
+        string QueryZipcode = string.Empty;
+
+        QueryZipcode = "select ID from Area where Name = N'" + DdlProvince.SelectedValue.ToString() + "'";
+        ms.GetOneData(QueryZipcode, sb);
+
+        Query = "select School from Account where zipcode ='" + sb.ToString() + "' and School not like N'%專家%' and School not like N'%专家%' and School not like N'%管理者%' group by School ";
+        if (!ms.GetAllColumnData(Query, data))
+        {
+            DdlSchool.Items.Add("None");
+            return;
+        }
+
+        /*if (data.Count == 0)
+        {
+            DdlSchool.Items.Add("None");
+            return;
+        }*/
+        //DdlSchool.Items.Add(Resources.Resource.DdlTypeSchoolname);
+        foreach (string[] province in data)
+        {
+            DdlSchool.Items.Add(province[0]);
+        }
+    }
+
+    private void RecoverPasswordFunc()
+    {
+        ManageSQL ms = new ManageSQL();
+        StringBuilder sb = new StringBuilder();
+
+        Query = "update Account set Password='0000' where School=N'" + DdlSchool.SelectedValue.ToString() + "'";
+        ms.WriteData(Query, sb);
+
+        Response.Redirect("SystemManagerIndex.aspx");
+    }
+    protected void BtnModify_Click(object sender, EventArgs e)
+    {
+        RecoverPasswordFunc();
+    }
+    protected void BtnCancel_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("SystemManagerIndex.aspx");
+    }
+    protected void DdlProvince_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DdlSchool.Items.Clear();
+        setSchoolName();
+    }
+}
